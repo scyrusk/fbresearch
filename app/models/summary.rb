bd22543962cb8057060ab1b0b4078e339836a686
scyrusk <sauvik@cmu.edu>
2014-10-01 12:53:16 -0400

    Summaries

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -13,7 +13,7 @@
 #
 
 class Summary < ActiveRecord::Base
-  belongs_to :publication, dependent: :destroy
+  belongs_to :publication
   has_many :votes
 
   validates_length_of :content, minimum: 50, maximum: 140, message: "Your summary must be between 50 and 140 characters"
